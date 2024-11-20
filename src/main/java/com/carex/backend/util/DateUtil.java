39fa0af624335bca8d391e86db5432471fe2272a
Caio <caio.ribeiro98@outlook.com>
2024-11-20 16:20:10 -0300

    convertDateToString method created.

--- src/main/java/com/carex/backend/util/DateUtil.java
+++ src/main/java/com/carex/backend/util/DateUtil.java
@@ -2,10 +2,18 @@ package com.carex.backend.util;
 
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Component
 public class DateUtil {
 
     public DateUtil() {
+    }
+
+    public String convertDateToString(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        return formatter.format(date);
     }
 
     public String applyStringDatePattern(String date) {
