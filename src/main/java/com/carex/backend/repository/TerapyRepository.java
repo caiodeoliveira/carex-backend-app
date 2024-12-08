034e41e5dddfb33eccef777a4a70fec3e77564aa
Caio <caio.ribeiro98@outlook.com>
2024-12-08 16:44:52 -0300

    find with sorting query method added.

--- src/main/java/com/carex/backend/repository/TerapyRepository.java
+++ src/main/java/com/carex/backend/repository/TerapyRepository.java
@@ -2,6 +2,11 @@ package com.carex.backend.repository;
 
 import com.carex.backend.entity.Terapy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TerapyRepository extends JpaRepository<Terapy, Long> {
+    @Query("SELECT t from Terapy t ORDER BY t.isAlternative DESC")
+    List<Terapy> findAllTerapySortingIsAlternativeFirst();
 }
