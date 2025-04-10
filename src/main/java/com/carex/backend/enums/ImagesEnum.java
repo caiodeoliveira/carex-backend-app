5bf82c530026f4bb78bd4aec912f7ee22b228c58
Caio <caio.ribeiro98@outlook.com>
2025-04-10 19:00:20 -0300

    Adjusts in image names

--- src/main/java/com/carex/backend/enums/ImagesEnum.java
+++ src/main/java/com/carex/backend/enums/ImagesEnum.java
@@ -1,13 +1,13 @@
 package com.carex.backend.enums;
 
 public enum ImagesEnum {
-    DRY_NEDLING("../../../assets/img/big/dry-nedling-big.png", "../../../assets/img/regular/dry-nedling-average.png", "../../../assets/img/small/dry-nedling-small.png", true, "Acupuntura"),
-    EAR_ACUNPUTURE("../../../assets/img/big/ear-acupunture-big.png", "../../../assets/img/regular/ear-acunputure-average.png", "../../../assets/img/small/ear-acunputure-small.png", true, "Auriculoterapia"),
-    MYOFACIAL_RELEASE("../../../assets/img/big/myofacial-release-big.png", "../../../assets/img/regular/myofacial-release-average.png", "../../../assets/img/small/myofacial-release-small.png", true, "Liberação Miofacial"),
-    SUCTION_CUP("../../../assets/img/big/suction-cup-big.png", "../../../assets/img/regular/suction-cup-average.png", "../../../assets/img/small/suction-cup-small.png", true, "Ventosaterapia"),
-    OBSTETRIC_PHYSIOTERAPY("../../../assets/img/big/obstetric-physioterapy-big.png", "../../../assets/img/regular/obstetric-physioterapy-average.png", "../../../assets/img/small/obstetric-physioterapy-small.png", false, "Fisioterapia Pélvica"),
-    PELVIC_PHYSIOTERAPY("../../../assets/img/big/pelvic-physioterapy-big.png", "../../../assets/img/regular/pelvic-physioterapy-average.png", "../../../assets/img/small/pelvic-physioterapy-small.png", false, "Fisioterapia Obstétrica"),
-    DOULAGE("../../../assets/img/big/doulage-big.png", "../../../assets/img/regular/doulage-average.png", "../../../assets/img/small/doulage-small.png", false, "Doulagem");
+    DRY_NEDLING("../../../assets/img/big/dry-nedling-big.png", "../../../assets/img/regular/dry-nedling-regular.png", "../../../assets/img/small/dry-nedling-small.png", true, "Acupuntura"),
+    EAR_ACUNPUTURE("../../../assets/img/big/ear-acupunture-big.png", "../../../assets/img/regular/ear-acupunture-regular.png", "../../../assets/img/small/ear-acupunture-small.png", true, "Auriculoterapia"),
+    MYOFACIAL_RELEASE("../../../assets/img/big/myofacial-release-big.png", "../../../assets/img/regular/myofacial-release-regular.png", "../../../assets/img/small/myofacial-release-small.png", true, "Liberação Miofacial"),
+    SUCTION_CUP("../../../assets/img/big/suction-cup-big.png", "../../../assets/img/regular/suction-cup-regular.png", "../../../assets/img/small/suction-cup-small.png", true, "Ventosaterapia"),
+    OBSTETRIC_PHYSIOTERAPY("../../../assets/img/big/obstetric-physioterapy-big.png", "../../../assets/img/regular/obstetric-physioterapy-regular.png", "../../../assets/img/small/obstetric-physioterapy-small.png", false, "Fisioterapia Obstétrica"),
+    PELVIC_PHYSIOTERAPY("../../../assets/img/big/pelvic-physioterapy-big.png", "../../../assets/img/regular/pelvic-physioterapy-regular.png", "../../../assets/img/small/pelvic-physioterapy-small.png", false, "Fisioterapia Pélvica"),
+    DOULAGE("../../../assets/img/big/doulage-big.png", "../../../assets/img/regular/doulage-regular.png", "../../../assets/img/small/doulage-small.png", false, "Doulagem");
 
     private String big;
     private String regular;
