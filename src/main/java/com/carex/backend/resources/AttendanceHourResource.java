22815432d978136c5279f83e3eeba410add31077
caio ribeiro <caio.ribeiro98@outlook.com>
2026-05-16 14:45:13 -0300

    (main): package change for resource classes

---
+++ src/main/java/com/carex/backend/resources/AttendanceHourResource.java
@@ -0,0 +1,28 @@
+package com.carex.backend.resources;
+
+import com.carex.backend.entity.AttendanceHour;
+import com.carex.backend.services.AttendanceHourService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/attendancehour")
+public class AttendanceHourResource {
+
+    @Autowired
+    AttendanceHourService attendanceHourService;
+
+    @GetMapping(value = "/allhoursavailable/{date}")
+    public ResponseEntity<List<AttendanceHour>> getAllProfessionalSchedule(@PathVariable Date date) {
+        List<AttendanceHour> professionalScheduleList = this.attendanceHourService.findAllAvailableAttendanceHourByDate(date);
+        return ResponseEntity.ok().body(professionalScheduleList);
+    }
+
+}
---
+++ src/main/java/com/carex/backend/resources/TerapyResource.java
@@ -0,0 +1,42 @@
+package com.carex.backend.resources;
+
+import com.carex.backend.dto.TerapyDTO;
+import com.carex.backend.entity.Terapy;
+import com.carex.backend.services.TerapyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/terapy")
+public class TerapyResource {
+
+    @Autowired
+    TerapyService terapyService;
+
+    @GetMapping(value = "/allTerapies")
+    public ResponseEntity<List<Terapy>> getAllTerapies() {
+        List<Terapy> terapies = this.terapyService.findAllTerapies();
+        return ResponseEntity.ok().body(terapies);
+    }
+
+    @GetMapping(value = "/names")
+    public ResponseEntity<List<String>> getAllTerapyNames() {
+        List<String> names = this.terapyService.findAllTerapyNames();
+        return ResponseEntity.ok().body(names);
+    }
+
+    @GetMapping(value = "/descriptions")
+    public ResponseEntity<List<String>> getAllTerapyDescriptions() {
+        List<String> descriptions = this.terapyService.findAllTerapyDescriptions();
+        return ResponseEntity.ok().body(descriptions);
+    }
+
+    @GetMapping(value = "/allterapiesdata/{size}")
+    public ResponseEntity<List<TerapyDTO>> getAllTerapyData(@PathVariable String size) {
+        List<TerapyDTO> terapyDataList = this.terapyService.findAllTerapiesWithImagesBySize(size);
+        return ResponseEntity.ok().body(terapyDataList);
+    }
+}
